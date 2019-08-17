55d14b58f58c1e31346d4635e2d32c14ce3d02e5
BAIPENG <2494156981@qq.com>
2019-08-17 17:59:29 +0800

    添加 Webpack 打包

---
+++ webpack.front.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    // entry: './src/logic_webgl/index.ts',
+    entry: {
+        logic_webgl: './src/front/main.ts'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'webpack_build/front')
+    },
+    // devtool: 'inline-source-map',                                  // 告诉 webpack 提取这些 source map，并内联到最终的 文件 中。
+    module: {
+        rules:[ 
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    // mode: 'development',
+    mode: 'production',
+    // optimization: {
+    //     usedExports: true
+    // },
+    plugins: [
+        new CleanWebpackPlugin()
+    ],
+}
